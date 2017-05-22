c76191a8c51260dd411847cc0e8d74b7c6f9a5a7
Ian <nobodyx647x@gmail.com>
2017-05-21 22:41:00 -0400

    Added audio demo

--- Test.go
+++ Test.go
@@ -10,11 +10,14 @@ import (
 	"strings"
 )
 
+
 func main() {
 	discord, _ := discordgo.New("Bot MTg5MTQ2MDg0NzE3NjI1MzQ0.DANL1A.4cLruFPliFxkd0r41pYB307_D1M")
 	discord.Open()
 	discord.ChannelMessageSend("104979971667197952", "hey there")
 	discord.AddHandler(messageCreate)
+	//server1 := new(server)
+
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
@@ -42,6 +45,11 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 }
 
-type Server struct {
-	//playlist:= []
+type server struct {
+	playlist []string
+	id       string
+}
+
+func (s *server) play() {
+
 }
