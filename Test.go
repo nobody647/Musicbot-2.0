701e7b06e2675871a4fab3c5852109c418c4b398
Ian <nobodyx647x@gmail.com>
2017-05-23 10:47:08 -0400

    Initial commit to devolop branch

--- Test.go
+++ Test.go
@@ -7,16 +7,22 @@ import (
 	"strings"
 	"os/signal"
 	"syscall"
+	"time"
+	"strconv"
+	"os/exec"
+	"fmt"
 )
 
-//var plm map[string][]string
+var plm map[string]*server
 
 func main() {
 	discord, _ := discordgo.New("Bot MTg5MTQ2MDg0NzE3NjI1MzQ0.DANL1A.4cLruFPliFxkd0r41pYB307_D1M")
 	discord.Open()
-	discord.ChannelMessageSend("104979971667197952", "hey there")
+	discord.ChannelMessageSend("104979971667197952", "*hello there*")
 
 	discord.AddHandler(messageCreate)
+
+	plm = make(map[string]*server)
 
 	sc := make(chan os.Signal, 1)
 	//noinspection ALL
@@ -24,7 +30,6 @@ func main() {
 	<-sc
 
 	discord.Close()
-	//plm = make(map[string][]string)
 
 }
 
@@ -45,14 +50,89 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	if strings.HasPrefix(m.Content, "!sr") {
-		dgv, _ := s.ChannelVoiceJoin("104979971667197952", "156887965392437250", false, false)
-		dgvoice.PlayAudioFile(dgv, "test.mp3")
-		//c, _ := s.State.Channel(m.ChannelID)
+		request := strings.TrimSpace(strings.TrimPrefix(m.Content,"!sr")) //Requested song/link
 
-		//plm[c.GuildID] = append(plm[c.GuildID], "wZZ7oFKsKzY") //Adds item to playlist
+		c, _ := s.State.Channel(m.ChannelID)
+		se := plm[c.GuildID] //Saves server locally
+
+		if se == nil { //Initializes server
+			se = new(server)
+			se.pl = make([]string, 0)
+			se.connect(s, c)
+		}
+
+		if !songExists(request){ //Download
+			go download(request)
+		}
+
+		se.pl = append(se.pl, request) //Adds item to playlist
+
+		plm[c.GuildID] = se
 
 	}
 
+	if strings.HasPrefix(m.Content, "!pll"){
+		c, _ := s.State.Channel(m.ChannelID)
+
+		s.ChannelMessageSend(m.ChannelID, strconv.Itoa(len(plm[c.GuildID].pl)))
+	}
 }
 
-//func play(c discordgo.Channel, )
+func (se *server) playLoop(s *discordgo.Session) {
+	for{
+		for len(se.pl)==0{
+			time.Sleep(time.Second*1)
+		}
+
+		for !songExists(se.pl[0]){
+			time.Sleep(time.Second*1)
+		}
+
+
+		se.playFile()
+		npl := make([]string, len(se.pl)-1)
+		for i := range se.pl{
+			if i==0{
+				continue
+			}
+			npl[i-1] = se.pl[i]
+		}
+		se.pl = npl
+
+	}
+}
+
+func (se *server)playFile() {
+	dgvoice.PlayAudioFile(se.dgv, se.pl[0]+".mp3")
+}
+
+func (se *server) connect(s *discordgo.Session, c *discordgo.Channel) {
+	g, _ := s.State.Guild(c.GuildID)
+
+	dgv, _ := s.ChannelVoiceJoin(g.ID, g.VoiceStates[0].ChannelID, false,false)
+	se.dgv = dgv
+	go se.playLoop(s)
+	return
+
+}
+
+type server struct{
+	dgv *discordgo.VoiceConnection
+	pl  []string
+
+}
+
+func download(s string){
+	cmd, _ := exec.Command("youtube-dl", " --extract-audio --audio-format mp3  --output \"%(id)s.%(ext)s\" "+s).Output()
+
+	fmt.Println(cmd)
+
+}
+
+func songExists(s string) bool{
+	if _, err := os.Stat(s); os.IsNotExist(err) { //Download
+		return false
+	}else{
+		return true
+	}
+}
