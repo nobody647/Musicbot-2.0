d057758086f54e180fa5d4ce4613be376822eb29
nobody647 <nobodyx647x@gmail.com>
2017-09-05 00:37:43 -0400

    skipall!

--- Main.go
+++ Main.go
@@ -93,6 +93,8 @@ func initCommands() {
 		if m.Content == "!skip" {
 			se.skip = true
 			se.pause = false
+		} else if strings.Contains(m.Content.ToLower(), "all") {
+			se.pl = se.pl[:0]
 		} else {
 			a := strings.TrimSpace(strings.TrimPrefix(m.Content, "!skip"))
 			i, err := strconv.Atoi(a)
